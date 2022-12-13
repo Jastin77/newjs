6290f39b87f87b8b12bacabdadef9f066ca3e9af
Jastin77 <jasurjonusmonov51@gmail.com>
2022-12-13 19:00:40 +0500

    khewgf

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const tbody = document.getElementById('tbody');
+
+
+let fruits = [
+    { namePro: Apple, weight: 120, price: 5 },
+    { namePro: Orange, weight: 100, price: 4.5 },
+    { namePro: Banana, weight: 90, price: 3.5 },
+    { namePro: Pier, weight: 120, price: 2.5 },
+    { namePro: Kiwi, weight: 150, price: 9 },
+    { namePro: Mango, weight: 320, price: 8 },
+    { namePro: Berry, weight: 520, price: 4 },
+    { namePro: strawberry, weight: 280, price: 3 },
+];
+
